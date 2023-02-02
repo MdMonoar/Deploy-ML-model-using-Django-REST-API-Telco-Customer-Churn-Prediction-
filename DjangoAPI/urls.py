251192cae20df8906a542de0d0ef8a3c00e06cbe
Md. Manuar Zaman <munna9757@gmail.com>
2023-02-02 19:46:02 +0600

    Fixed the homepage related issue. Customer information form will show in the home page now. Changed the title of form and status page. 7:45pm 02/02/2023

--- DjangoAPI/urls.py
+++ DjangoAPI/urls.py
@@ -6,5 +6,6 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('form/', views.FormView, name='form'),
+    # path('form/', views.FormView, name='form'),
+    path('', views.FormView, name='form'), # used blank '' inplace of 'form/' to make the form view as home page
 ]
