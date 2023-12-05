b1d73900c42869880240716dc359cb56cb1641e9
Md. Manuar Zaman <munna9757@gmail.com>
2023-12-06 00:27:00 +0600

    deploy attept on render 1

--- DeployChurn1/settings.py
+++ DeployChurn1/settings.py
@@ -19,14 +19,30 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ds-jzavdzv&%g!zum82ie#i30jp2uc7d+-#yo*uw^xz#=wh_^)'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # ... commenting for deployment, uncomment when running in development server 
+## {
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-ds-jzavdzv&%g!zum82ie#i30jp2uc7d+-#yo*uw^xz#=wh_^)'
 
-ALLOWED_HOSTS = []
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
+# ALLOWED_HOSTS = []
+## }
+
+
+## ... comment the following block before running in development server
+# {
+import os
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key') # from render
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+# }
 
 # Application definition
 
@@ -43,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +137,28 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+## ... use only in production
+# {
+# Following settings only make sense on production and may break 
+# development environments.
+
+# Tell Django to copy statics to the `staticfiles` directory
+# in your application directory on Render.
+    
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #from render
+
+    # following block is from witenoise official for django 4.2+
+    STORAGES = {
+        # ...
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        },
+    }
+# }
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
